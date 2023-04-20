0eaf82a735083c84e2b003d33160d425241d7921
Ukesh Kumar <ukeshsaravanan@gmail.com>
2023-04-20 15:52:56 +0530

    Initial Commit

---
+++ ChatAppSwiftUI/ChatAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppSwiftUIApp.swift
+//  ChatAppSwiftUI
+//
+//  Created by UKESH KUMAR on 20/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
